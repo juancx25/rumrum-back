8ccabc63b040c717e936051ce88350d68d8d936c
juancouselo25 <juancouselo25@gmail.com>
2023-10-01 12:45:38 -0300

    Basic AP and website working

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from machine import Pin
-from time import sleep
+try:
+  import usocket as socket
+except:
+  import socket
 
-LED_BUILTIN = Pin(2, Pin.OUT)
+def web_page():
+    with open('./site/index.html') as fh:
+        data = fh.read()
+    return data
 
-while(1):
-    LED_BUILTIN.on(); 
-    sleep(1);
-    LED_BUILTIN.off();
-    sleep(1);
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  #print('Content = %s' % str(request))
+  response = web_page()
+  conn.send(response)
+  conn.close()
